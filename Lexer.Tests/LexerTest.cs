2ee98710ccdd808771a9c5459b9fa1cbe35f9faa
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-03 21:55:28 +0100

    Add failing test for more extensive code example.

--- Lexer.Tests/LexerTest.cs
+++ Lexer.Tests/LexerTest.cs
@@ -7,12 +7,12 @@ namespace LexerNS.Tests
     public class LexerTest
     {
         [TestCase]
-        public void TestNextToken()
+        public void TestNextToken_ReducedSet()
         {
             // Arrange
             var input = "=+(){},;";
 
-            var tests = new (TokenType expectedType, string expectedLiteral)[] {
+            var tests = new(TokenType expectedType, string expectedLiteral)[] {
                 ( Token.ASSIGN, "=" ),
                 ( Token.PLUS, "+" ),
                 ( Token.LPAREN, "(" ),
@@ -24,6 +24,71 @@ namespace LexerNS.Tests
                 ( Token.EOF, "" )
             };
 
+            // Act and Assert
+            TestNextToken(input, tests);
+        }
+
+        [TestCase]
+        public void TestNextToken_RealMonkey()
+        {
+            // Arrange
+            var input = string.Join(System.Environment.NewLine, new[] {
+                "let five = 5;",
+                "let ten = 10;",
+                "",
+                "let add = fn(x, y) {",
+                "  x + y;",
+                "};",
+                "",
+                "let result = add(five, ten);"
+            });
+
+            var tests = new(TokenType expectedType, string expectedLiteral)[] {
+                ( Token.LET, "let" ),
+                ( Token.IDENT, "five" ),
+                ( Token.ASSIGN, "=" ),
+                ( Token.INT, "5" ),
+                ( Token.SEMICOLON, ";" ),
+                ( Token.LET, "let" ),
+                ( Token.IDENT, "ten" ),
+                ( Token.ASSIGN, "=" ),
+                ( Token.INT, "10" ),
+                ( Token.SEMICOLON, ";" ),
+                ( Token.LET, "let" ),
+                ( Token.IDENT, "add" ),
+                ( Token.ASSIGN, "=" ),
+                ( Token.FUNCTION, "fn" ),
+                ( Token.LPAREN, "(" ),
+                ( Token.IDENT, "x" ),
+                ( Token.COMMA, "," ),
+                ( Token.IDENT, "y" ),
+                ( Token.RPAREN, ")" ),
+                ( Token.LBRACE, "{" ),
+                ( Token.IDENT, "x" ),
+                ( Token.PLUS, "+" ),
+                ( Token.IDENT, "y" ),
+                ( Token.SEMICOLON, ";" ),
+                ( Token.RBRACE, "}" ),
+                ( Token.SEMICOLON, ";" ),
+                ( Token.LET, "let" ),
+                ( Token.IDENT, "result" ),
+                ( Token.ASSIGN, "=" ),
+                ( Token.IDENT, "add" ),
+                ( Token.LPAREN, "(" ),
+                ( Token.IDENT, "five" ),
+                ( Token.COMMA, "," ),
+                ( Token.IDENT, "ten" ),
+                ( Token.RPAREN, ")" ),
+                ( Token.SEMICOLON, ";" ),
+                ( Token.EOF, "" )
+            };
+
+            // Act and Assert
+            TestNextToken(input, tests);
+        }
+
+        private static void TestNextToken(string input, (TokenType expectedType, string expectedLiteral)[] tests)
+        {
             // Act
             var l = new Lexer(input);
             int testIndex = 0;
