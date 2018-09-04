c1e2e54fafd7609a1840c38c957608b520ca6def
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-04 21:56:19 +0100

    Add initial ProgramRoot node.

---
+++ AbstractSyntaxTree/ProgramRoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace AbstractSyntaxTree
+{
+    /// <summary>
+    /// NOTE: Changed name to ProgramRoot so does not get
+    /// confused with a 'Program' entry class.
+    /// </summary>
+    class ProgramRoot : INode
+    {
+        private IStatement[] statements;
+
+        public string TokenLiteral()
+        {
+            if (!statements.Any()) return "";
+            return statements[0].TokenLiteral();
+        }
+    }
+}
