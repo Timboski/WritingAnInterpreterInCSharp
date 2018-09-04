403859764e4cb45add4dbdb6f8a1fc3ad6ae7dd5
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-04 22:07:43 +0100

    Add LetStatement and Identifier.

---
+++ AbstractSyntaxTree/Identifier.cs
@@ -0,0 +1,12 @@
+﻿using TokenNS;
+
+namespace AbstractSyntaxTree
+{
+    internal class Identifier : IExpression
+    {
+        Token token; // The IDENT token.
+        string value;
+
+        public string TokenLiteral() => token.Literal;
+    }
+}
---
+++ AbstractSyntaxTree/LetStatement.cs
@@ -0,0 +1,13 @@
+﻿using TokenNS;
+
+namespace AbstractSyntaxTree
+{
+    class LetStatement : IStatement
+    {
+        private Token token; // The LET token.
+        private Identifier name;
+        private IExpression value;
+
+        public string TokenLiteral() => token.Literal;
+    }
+}
