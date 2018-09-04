57a6a91887526bca95ba4c013a67c0de14b071c3
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-04 22:20:40 +0100

    Add Parser project and initial implementation.

---
+++ Parser/Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AbstractSyntaxTree;
+using LexerNS;
+using TokenNS;
+
+namespace ParserNS
+{
+    public class Parser
+    {
+        private Lexer lexer;
+        private Token curToken;
+        private Token peekToken;
+
+        public Parser(Lexer lexer)
+        {
+            this.lexer = lexer;
+
+            // Read two tokens, so curToken and peekToken are both set.
+            NextToken();
+            NextToken();
+        }
+
+        public ProgramRoot ParseProgram() => null;
+
+        private void NextToken()
+        {
+            curToken = peekToken;
+            peekToken = lexer.NextToken();
+        }
+    }
+}
