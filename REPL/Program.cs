1a51578a89c8a1b19093cb4f4b7da36d843548db
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-04 21:31:18 +0100

    Add initial REPL implementation.

--- REPL/Program.cs
+++ REPL/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LexerNS;
+using System;
+using TokenNS;
 
 namespace REPL
 {
@@ -6,7 +8,20 @@ namespace REPL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello {Environment.UserName}! This is the Monkey programming language!");
+            Console.WriteLine("Feel free to type in commands");
+
+            while (true)
+            {
+                Console.Write(">>");
+                var input = Console.ReadLine();
+                var lexer = new Lexer(input);
+
+                for (var tok = lexer.NextToken(); tok.Type != Token.EOF; tok = lexer.NextToken())
+                {
+                    Console.WriteLine($"{{Type:{(string)tok.Type} literal:{tok.Literal}}}");
+                }
+            }
         }
     }
 }
