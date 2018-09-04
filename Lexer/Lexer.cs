8743463ed01aa1cb476d60d2167d42b95d263812
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-04 20:37:49 +0100

    Extend Lexer to support extra operators.

--- Lexer/Lexer.cs
+++ Lexer/Lexer.cs
@@ -53,6 +53,36 @@ namespace LexerNS
                         token = new Token(Token.PLUS, ch);
                         break;
                     }
+                case '-':
+                    {
+                        token = new Token(Token.MINUS, ch);
+                        break;
+                    }
+                case '!':
+                    {
+                        token = new Token(Token.BANG, ch);
+                        break;
+                    }
+                case '/':
+                    {
+                        token = new Token(Token.SLASH, ch);
+                        break;
+                    }
+                case '*':
+                    {
+                        token = new Token(Token.ASTERISK, ch);
+                        break;
+                    }
+                case '<':
+                    {
+                        token = new Token(Token.LT, ch);
+                        break;
+                    }
+                case '>':
+                    {
+                        token = new Token(Token.GT, ch);
+                        break;
+                    }
                 case '{':
                     {
                         token = new Token(Token.LBRACE, ch);
