76aa48335edde9b7af2a11cc9ab01f7b812e8078
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-04 21:12:20 +0100

    Add REPL project.

---
+++ REPL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace REPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
