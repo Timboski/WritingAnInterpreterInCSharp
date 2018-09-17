d74d148a91e8900960ba62cb59a482cbefb7e088
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-17 21:22:38 +0100

    Add ExpressionStatement to AST.

---
+++ AbstractSyntaxTree/ExpressionStatement.cs
@@ -0,0 +1,14 @@
+﻿using TokenNS;
+
+namespace AbstractSyntaxTree
+{
+    public class ExpressionStatement : IStatement
+    {
+        public ExpressionStatement(Token token) => Token = token;
+
+        public Token Token { get; } // The RETURN token.
+        public IExpression Expression { get; set; }
+
+        public string TokenLiteral() => Token.Literal;
+    }
+}
