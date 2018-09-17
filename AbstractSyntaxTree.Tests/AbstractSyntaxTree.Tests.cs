2b6c593eb280dc9fe424b20d2d8cd6a5ff6c9b4f
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-17 22:13:11 +0100

    Add unit test for reconstructing program from AST

--- AbstractSyntaxTree.Tests/AbstractSyntaxTree.Tests.cs
+++ AbstractSyntaxTree.Tests/AbstractSyntaxTree.Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using TokenNS;
 
 namespace AbstractSyntaxTree.Tests
 {
@@ -9,10 +10,25 @@ namespace AbstractSyntaxTree.Tests
         public void TestString()
         {
             // Arrange
+            var lhsIdentifierToken = new Token(Token.IDENT, "myVar");
+            var rhsIdentifierToken = new Token(Token.IDENT, "anotherVar");
+            var letToken = new Token(Token.LET, "let");
+
+            var letStatement = new LetStatement(letToken)
+            {
+                Name = new Identifier(lhsIdentifierToken),
+                Value = new Identifier(rhsIdentifierToken)
+            };
+
+            var program = new ProgramRoot();
+            program.Statements.Add(letStatement);
 
             // Act
+            var programString = program.String();
 
             // Assert
+            var expected = "let myVar = anotherVar;";
+            Assert.AreEqual(expected, programString);
         }
     }
 }
