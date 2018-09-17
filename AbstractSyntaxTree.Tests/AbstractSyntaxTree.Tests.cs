10043d658d48ef529179531ba30591dcb6a7913b
Tim Boyle <tim.boyle@sharp-ls.eu>
2018-09-17 21:47:30 +0100

    Add unit test project for AST.

---
+++ AbstractSyntaxTree.Tests/AbstractSyntaxTree.Tests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AbstractSyntaxTree.Tests
+{
+    [TestFixture]
+    public class AbstractSyntaxTreeTest
+    {
+        [TestCase]
+        public void TestString()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
